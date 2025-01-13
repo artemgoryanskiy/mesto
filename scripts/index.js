0a5565d05b384728c859eb25177f2e4c1e81a96f
Артем Горянский <artemgoranskij@MacBook-Pro-Artem.local>
2025-01-13 21:30:13 +0300

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function deleteCard(index) {
+  initialCards.splice(index, 1);
+}
+
+function createCard(cardData, deleteHandler) {
+  const template = document.querySelector('#card-template').content;
+
+  const card = template.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__title').textContent = cardData.name;
+
+  const cardDeleteButton = card.querySelector('.card__delete-button');
+
+  cardDeleteButton.addEventListener('click', () => {
+    deleteHandler();
+    cardDeleteButton.parentElement.remove();
+  })
+
+  return card;
+}
+
+initialCards.map((card, index) => document.querySelector('.places__list').append(createCard(card, () => deleteCard(index))))
+
+
+
