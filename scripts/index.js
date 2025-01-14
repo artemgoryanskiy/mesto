651d88dc6be490d94b8559ebbcf61edb92d82101
Artem Goryanskiy <artemgoryanskiy@yandex.ru>
2025-01-14 10:59:37 +0300

    fixed-2

--- scripts/index.js
+++ scripts/index.js
@@ -12,21 +12,24 @@ function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-function createCard(cardData) {
+function createCard(cardData, onDeleteCard) {
   const template = document.querySelector("#card-template").content;
   const card = template.querySelector(".card").cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
+  const image = card.querySelector(".card__image");
+  const title = card.querySelector(".card__title");
 
-  card.querySelector(".card__image").src = cardData.link;
-  card.querySelector(".card__image").alt = `Фотография места: ${cardData.name}`;
-  card.querySelector(".card__title").textContent = cardData.name;
+  image.src = cardData.link;
+  image.alt = `Фотография места: ${cardData.name}`;
+  title.textContent = cardData.name;
+  
   deleteButton.addEventListener("click", () => {
-    deleteCard(card);
+    onDeleteCard(card);
   });
 
   return card;
 }
 
 initialCards.forEach((card) =>
-  document.querySelector(".places__list").append(createCard(card))
+  document.querySelector(".places__list").append(createCard(card, deleteCard))
 );
